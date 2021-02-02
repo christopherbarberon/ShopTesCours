0bc00f74bdf31757ac26d09d557362fd0260fc76
Léna <lenapancher@gmail.com>
2021-02-02 13:50:41 +0100

    panier : ajout d'article

--- js/app.js
+++ js/app.js
@@ -1,43 +1,39 @@
-
 function addToCartButtons() {
     var addToCartButtons = document.getElementsByClassName('add-to-cart');
     for (var i = 0; i < addToCartButtons.length; i++) {
-        var button = addToCartButtons[i];
+        var button = addToCartButtons[i]
         button.addEventListener('click', event => {
             console.log("id : " + event.target.getAttribute('data-id'));
-            console.log("click");
-            addToCartClicked(event)
-            //addItemToCart(event);
-        })
+            addToCartClicked(event);
+        }) 
     }
 }
 
 addToCartButtons();
-
 
 function addToCartClicked(event) {
     var button = event.target;
     var dataId = button.getAttribute('data-id');
     var shopItem = button.parentNode;
     var title = shopItem.getElementsByTagName('h4')[0].innerText;
-    console.log(title);
     var price = shopItem.getElementsByClassName('discount')[0].innerText;
-    console.log(price);
-    addItemToCart(title, price);
+    var getImg = shopItem.parentNode;
+    var img = getImg.getElementsByTagName('img')[0];
+    var src = img.getAttribute('src');
+
+    addItemToCart(src, title, price);
+    alert('Votre produit a été ajouté avec succès');
 }
 
-
-function addItemToCart(title, price) {
-    var cartTableBody = getElementsByTagName('tbody');
-    console.log(cartTableBody);
-    var newTr = createElement('tr');
-    cartTableBody.appendChild(newTr);
-
-    var newTdTitle = createElement('td');
+function addItemToCart(img, title, price) {
+    var cartTableBody = document.getElementsByTagName('tbody')[0];
+    var newTr = cartTableBody.insertRow(-1);
+    var newTdImg = newTr.insertCell(0);
+    var newSrc = document.createElement('img');
+    newSrc.src = img;
+    newTdImg.appendChild(newSrc);
+    var newTdTitle = newTr.insertCell(-1);
     newTdTitle.innerHTML = title;
-    newTdTitle = newTr.insertCell(0);
-
-    var newTdPrice = createElement('td');
+    var newTdPrice = newTr.insertCell(-1);
     newTdPrice.innerHTML = price;
-    newTdPrice = newTr.insertCell(-1);
 }
